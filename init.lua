a9a5697b6b54b154a96d1be86991d4fa82216af3
carlosiborra <charlie.iborra@gmail.com>
2024-12-15 16:48:31 +0100

    Update to dotfiles structure

--- init.lua
+++ init.lua
@@ -1,35 +1,3 @@
-vim.g.mapleader = " "  -- Substitute leader key '\' to a space
-
--- Bootstrap Lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Setup lazy.nvim
-require("lazy").setup("plugins")
-
--- Basic settings
-vim.opt.number = true          -- Show line numbers
---vim.opt.relativenumber = true  -- Show relative line numbers
-vim.opt.wrap = false           -- Disable line wrapping
-vim.opt.tabstop = 4            -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 4         -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true       -- Use spaces instead of tabs
-vim.opt.smartindent = true     -- Smart indentation
-vim.opt.cursorline = true      -- Highlight the current line
-vim.opt.termguicolors = true   -- Enable 24-bit RGB colors
-vim.opt.mouse = 'a'            -- Enable mouse support
-vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
-vim.opt.ignorecase = true      -- Ignore case when searching
-vim.opt.smartcase = true       -- Override ignorecase if search contains capitals
-vim.opt.updatetime = 300       -- Faster completion
-vim.opt.signcolumn = 'yes'     -- Always show the signcolumn
+require("config.config")
+require("config.lazy-config")
+require("keymaps.keymaps")
---
+++ lua/keymaps/keymaps.lua
@@ -0,0 +1,6 @@
+-- Key mappings for moving between splits
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = "Move to the left split" })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = "Move to the right split" })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = "Move to the below split" })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = "Move to the above split" })
+
